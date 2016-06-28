b9d57d5019708292b6f0ccb7fbf8fbfc70b79ce9
Robert Heel <Robert.Heel@gmx.de>
2016-06-28 22:31:19 +0200

    Add: Static TS for standard installations.

--- ext_tables.php
+++ ext_tables.php
@@ -1,5 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
+t3lib_extMgm::addStaticFile($_EXTKEY,'static/standard/', 'PDF page');
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/templavoila/', 'PDF page with templavoila');
 ?>
